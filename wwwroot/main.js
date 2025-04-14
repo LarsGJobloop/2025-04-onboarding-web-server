26fcc38e8a5bdd3aacf0964f4294410b29b45369
zabronax <zabronax@gmail.com>
2025-04-14 14:19:18 +0200

    setup static file serving

--- wwwroot/main.js
+++ wwwroot/main.js
@@ -1,2 +1,14 @@
 console.log("Script loaded...")
 
+// First querry the server for all quotes
+let response = await fetch("/quotes")
+let quotes = await response.json()
+
+// Then write them to the Document
+let qouteList = document.querySelector("#quotes")
+
+for (let quote of quotes) {
+  let newElement = document.createElement("li")
+  newElement.textContent = quote.content
+  qouteList.append(newElement)
+}
